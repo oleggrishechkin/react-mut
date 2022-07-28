95da77d385def0f2a0a68837660ed33075344fd5
Oleg Grishechkin <oleggrishechkin@gmail.com>
2022-07-29 00:04:24 +0300

    Add shim hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { useCallback } from 'react';
-import { useSyncExternalStore } from 'use-sync-external-store/shim';
+import { useCallback, useEffect, useLayoutEffect, useMemo, useState, useSyncExternalStore } from 'react';
 
-type ObjectType = Record<any, any> | any[] | Set<any> | Map<any, any> | WeakSet<any> | WeakMap<any, any>;
+type AnyObject = any;
 
 type Version = any;
 
@@ -12,100 +11,157 @@ type Subscriber = {
     version: Version;
 };
 
-let batchedObjects: Set<ObjectType> | null = null;
+let batchedObjects: Set<AnyObject> | null = null;
 
-const objectSubscribers = new WeakMap<ObjectType, Set<Subscriber>>();
+const objectSubscribers = new WeakMap<AnyObject, Set<Subscriber>>();
 
-const objectVersion = new WeakMap<ObjectType, Version>();
+const objectVersion = new WeakMap<AnyObject, Version>();
 
-const isObject = <T>(object: T): boolean => typeof object === 'object' && object !== null;
+const isObject = <T>(object: T) => typeof object === 'object' && object !== null;
 
 export const ver = <T>(object: T): T | Version => (isObject(object) ? objectVersion.get(object) || object : object);
 
 export const sub = <T>(object: T, callback: () => void): (() => void) => {
-    if (!isObject(object)) {
-        return () => {};
-    }
+    if (isObject(object)) {
+        const subscriber = { callback, version: ver(object) };
+        const subscribers = objectSubscribers.get(object);
 
-    const subscriber = { callback: callback, version: ver(object) };
-    let subscribers = objectSubscribers.get(object);
-
-    if (subscribers) {
-        subscribers.add(subscriber);
-    } else {
-        subscribers = new Set([subscriber]);
-        objectSubscribers.set(object, subscribers);
-    }
-
-    return () => {
-        if (subscribers!.size === 1) {
-            objectSubscribers.delete(object);
-            objectVersion.delete(object);
-
-            return;
+        if (subscribers) {
+            subscribers.add(subscriber);
+        } else {
+            objectSubscribers.set(object, new Set([subscriber]));
         }
 
-        subscribers!.delete(subscriber);
-    };
-};
-
-export const mut = <T>(object: T): T => {
-    const isBatched = batchedObjects !== null;
-
-    if (batchedObjects === null) {
-        batchedObjects = new Set([selectorObject]);
-        objectVersion.set(selectorObject, {});
-    }
-
-    if (isObject(object) && objectSubscribers.has(object!)) {
-        batchedObjects.add(object!);
-        objectVersion.set(object!, {});
-    }
-
-    if (isBatched) {
-        return object;
-    }
-
-    Promise.resolve().then(() => {
-        const objects = batchedObjects!;
-
-        batchedObjects = null;
-        objects.forEach((object) => {
+        return () => {
             const subscribers = objectSubscribers.get(object);
 
             if (subscribers) {
-                const version = ver(object);
+                if (subscribers.size === 1) {
+                    objectSubscribers.delete(object);
+                    objectVersion.delete(object);
+                } else {
+                    subscribers.delete(subscriber);
+                }
+            }
+        };
+    }
 
-                subscribers.forEach((subscriber) => {
-                    if (subscriber.version !== version) {
-                        subscriber.version = version;
-                        subscriber.callback();
+    return () => {};
+};
+
+export const mut = <T>(object: T): T => {
+    if (batchedObjects === null) {
+        batchedObjects = new Set();
+        Promise.resolve().then(() => {
+            if (batchedObjects !== null) {
+                const objects = batchedObjects;
+
+                batchedObjects = null;
+                objects.forEach((object) => {
+                    const subscribers = objectSubscribers.get(object);
+
+                    if (subscribers) {
+                        const version = ver(object);
+
+                        subscribers.forEach((subscriber) => {
+                            if (subscriber.version !== version) {
+                                subscriber.version = version;
+                                subscriber.callback();
+                            }
+                        });
                     }
                 });
             }
         });
-    });
+    }
+
+    if (objectSubscribers.has(selectorObject)) {
+        batchedObjects.add(selectorObject);
+        objectVersion.set(selectorObject, {});
+    }
+
+    if (isObject(object) && objectSubscribers.has(object)) {
+        batchedObjects.add(object);
+        objectVersion.set(object, {});
+    }
 
     return object;
 };
 
-export const useMut = <T>(object: T): T => {
-    useSyncExternalStore(
-        useCallback((handleChange: () => void) => sub(object, handleChange), [object]),
-        () => ver(object)
-    );
+const useIsomorphicLayoutEffect = typeof window === 'undefined' ? useEffect : useLayoutEffect;
 
-    return object;
-};
+const useSyncExternalStoreShim =
+    typeof useSyncExternalStore === 'undefined'
+        ? <T>(subscribe: (callback: () => void) => () => void, getSnapshot: () => T): T => {
+              const value = getSnapshot();
+              const [{ inst }, forceUpdate] = useState({ inst: { value, getSnapshot } });
+
+              useIsomorphicLayoutEffect(() => {
+                  inst.value = value;
+                  inst.getSnapshot = getSnapshot;
+
+                  if (inst.value !== inst.getSnapshot()) {
+                      forceUpdate({ inst });
+                  }
+              }, [subscribe, value, getSnapshot]);
+              useEffect(() => {
+                  if (inst.value !== inst.getSnapshot()) {
+                      forceUpdate({ inst });
+                  }
+
+                  return subscribe(() => {
+                      if (inst.value !== inst.getSnapshot()) {
+                          forceUpdate({ inst });
+                      }
+                  });
+              }, [inst, subscribe]);
+
+              return value;
+          }
+        : useSyncExternalStore;
+
+export const useMut =
+    typeof useSyncExternalStore === undefined
+        ? <T>(object: T): T => {
+              const [, setState] = useState(() => ver(object));
+
+              useEffect(() => {
+                  setState(ver(object));
+
+                  return sub(object, () => setState(ver(object)));
+              }, [object]);
+
+              return object;
+          }
+        : <T>(object: T): T => {
+              useSyncExternalStoreShim(
+                  useCallback((handleChange) => sub(object, handleChange), [object]),
+                  () => ver(object)
+              );
+
+              return object;
+          };
 
 export const useSel = <T>(sel: () => T): T =>
-    useSyncExternalStore(
-        useCallback((handleChange: () => void) => sub(selectorObject, handleChange), []),
-        sel
+    useSyncExternalStoreShim(
+        useCallback((handleChange) => sub(selectorObject, handleChange), []),
+        useMemo(() => {
+            let currentVersion: Version;
+            let value: T;
+
+            return () => {
+                const version = ver(selectorObject);
+
+                if (version === currentVersion) {
+                    return value;
+                }
+
+                currentVersion = version;
+                value = sel();
+
+                return value;
+            };
+        }, [sel])
     );
 
-export const useMutSel = <T>(sel: () => T): T => {
-    const object = useSel(sel);
-
-    return useMut(object);
-};
+export const useMutSel = <T>(sel: () => T): T => useMut(useSel(sel));
